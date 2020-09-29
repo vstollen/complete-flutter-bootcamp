2f329c77239c105587807d251e6dd503c170f479
vstollen <vstollen@gmail.com>
2020-09-29 14:01:07 +0200

    Add NetworkImage - 3.2

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello World!"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image.network(
+              'https://i.ytimg.com/vi/dXM6i5-sdVg/maxresdefault.jpg'),
+        ),
+      ),
     ),
-  ));
+  );
 }
